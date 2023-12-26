ef4126134a598a17ce908592fb4a1a9b3e7f4359
Tharushiwijesinghe <134124219+Tharushiwijesinghe@users.noreply.github.com>
2023-12-26 11:40:51 +0530

    Create Dog.java

---
+++ Java-oop/Q2/Dog.java
@@ -0,0 +1,29 @@
+public class Dog {
+  private String breed;
+  private String name;
+
+  public Dog(String name,String breed) {
+      this.name = name;
+      this.breed = breed;
+
+  }
+      public String getName()
+      {
+          return name;
+      }
+      public void setName(String name)
+      {
+        this.name = name;
+      }
+      public String getBreed()
+      {
+          return breed;
+      }
+      public void setBreed(String breed)
+      {
+          this.breed =breed;
+      }
+
+
+}
+
